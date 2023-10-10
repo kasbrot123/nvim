6d1a9e59f47bade50f964b1d2ffaab79deaaa3bd
Michael T <michael@computer>
2023-10-10 10:44:46 +0200

    nvim tree no resize when entering new file

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -29,6 +29,7 @@ nvimtree.setup({
     -- window splits
     actions = {
         open_file = {
+            resize_window = false,
             window_picker = {
                 enable = false,
             },
