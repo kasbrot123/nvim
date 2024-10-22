26fec24c17f6a6b1a7b64d0bb781fab4a4aefec7
Michael T. <michael@computer>
2024-10-22 19:00:16 +0200

    modified lualine, now with more useful information

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -14,4 +14,28 @@ lualine.setup({
         section_separators = { left = '', right = ''},
         component_separators = { left = '|', right = '|'},
     },
+    sections = {
+        -- Default, when your window is active
+        -- lualine_a = {'mode'},
+        -- lualine_b = {'branch', 'diff', 'diagnostics'},
+        -- lualine_c = {'filename'},
+        -- lualine_x = {'encoding', 'fileformat', 'filetype'},
+        -- lualine_y = {'progress'},
+        -- lualine_z = {'location'}
+
+        lualine_b = {'branch', 'diagnostics'},
+        lualine_x = {'encoding', 'fileformat', 'filesize'},
+        lualine_z = {'location'}
+    },
+    inactive_sections = {
+        -- Default, when you have a split window which is inactive
+        -- lualine_a = {},
+        -- lualine_b = {},
+        -- lualine_c = {'filename'},
+        -- lualine_x = {'location'},
+        -- lualine_y = {},
+        -- lualine_z = {}
+
+        lualine_x = {'encoding', 'fileformat', 'filesize'},
+    }
 })
