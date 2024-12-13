491994dddb77cab6b75127979ab8fae614b46092
Michael T. <michael@computer>
2024-12-13 06:42:39 +0100

    harpoon file mappings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -17,10 +17,10 @@ vim.keymap.set("n", "<leader>r", function() harpoon.ui:toggle_quick_menu(harpoon
 
 -- rest I dont need
 
--- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
--- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
--- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
--- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<space>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<space>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<space>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<space>4", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
