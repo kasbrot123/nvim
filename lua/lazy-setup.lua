30b3805703a09bd45d789f7940a575d586b983a3
Michael T. <michael@computer>
2025-04-27 21:47:53 +0200

    added incremental visual with treesitter

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -206,6 +206,16 @@ require("lazy").setup({
                 indent = { enable = false },
                 autotag = { enable = true }, -- enable autotagging (w/ nvim-ts-autotag plugin)
                 auto_install = false,
+
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<CR>",
+                        node_incremental = "<CR>",
+                        scope_incremental = false,
+                        node_decremental = "<bs>",
+                    },
+                },
             })
         end,
     },
