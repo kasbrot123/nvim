9aee7a937c18d0b7946aa283cf0afa5c21e93b67
Michael T. <michael@computer>
2025-11-28 18:20:36 +0100

    added execute shortcuts and added imports to python template

--- templates/python.py
+++ templates/python.py
@@ -7,10 +7,14 @@ File: {{_file_name_}}.py
 Created by: {{_author_}}
 Date: {{_date_}}
 
+Description:
+
+
 """
 
 import os
 import sys
+import glob
 import numpy as np 
 from matplotlib import pyplot as plt
 
