a4b6819c9da56611430f860bfbba95bff46a78d0
Michael T. <michael@computer>
2026-02-14 10:31:43 +0100

    removed autopair config junk typescript etc.

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -46,6 +46,9 @@ require("lazy").setup({
       -- tables in markdown
       { "dhruvasagar/vim-table-mode" },
 
+      -- auto closing
+      { 'windwp/nvim-autopairs', event = "InsertEnter", config = true },
+
       -- Colorschemes
       {
           'bluz71/vim-nightfly-colors',
@@ -358,31 +361,6 @@ require("lazy").setup({
         end,
     },
 
-
-    -- auto closing
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        dependencies = {
-            -- after nvim-treesitter
-            "windwp/nvim-ts-autotag"
-        },
-        config = function()
-            local autopairs = require("nvim-autopairs")
-            autopairs.setup({
-                check_ts = true, -- enable treesitter
-                ts_config = {
-                    lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-                    javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-                    java = false, -- don't check treesitter on java
-                },
-            })
-            local cmp = require("cmp")
-            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-            -- make autopairs and completion work together
-            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        end,
-    },
 
     {
         'glepnir/template.nvim',
